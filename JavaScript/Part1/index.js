a3653174c254927f3f037e2556433d3ef5d4ba93
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-08-24 20:48:48 +0530

    part1

---
+++ JavaScript/Part1/index.js
@@ -0,0 +1,28 @@
+let age = 23;
+age + 2;
+console.log(age)
+
+let hindi = 80;
+let english = 90;
+let math = 100;
+let avg = (hindi + english + math) / 3;
+console.log(avg);
+
+let a = 5
+a += 2;
+console.log(a)
+// age + 1 OR age += 1 == 7;
+
+let year = 2000;
+console.log(year);
+year++;
+year++;
+console.log(year);
+year--;
+year--;
+console.log(year);
+
+//Identifiers :
+//let camelCase;
+//let snake_case;
+//let PascalCase;
