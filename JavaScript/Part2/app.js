a06b8a2d3364777ba4f8b6ac28663a61f85a40df
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-08-25 18:05:06 +0530

    Part2

---
+++ JavaScript/Part2/app.js
@@ -0,0 +1,180 @@
+// console.log("Hello World");
+// console.log("Apna College");
+// let a = 10;
+// let b = 5;
+// console.log("Sum is :", a + b);
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+// let output = "The total price is : " + (pencilPrice + eraserPrice) + "Rupees.";
+// let output = `The total price is : ${pencilPrice + eraserPrice} Rupees`;
+// console.log(`The total price is : ${pencilPrice + eraserPrice} Rupees`);
+// let age = 23;
+// console.log(age == 18);
+// console.log(5 != 7);
+// console.log('A' > 'a');
+// if (age >= 18) {
+//     console.log("You are an Adult.");
+//     console.log("You are eligible to vote.");
+//     console.log("You are eligible for DL.");
+// } else {
+//     console.log("You are not an adult");
+// }
+
+// let btn = "Unfollow";
+// if (btn == "Follow") {
+//     console.log("You are following")
+// } else {
+//     console.log("You need to follow to access posts.")
+// }
+
+// let trafficLight = "Red";
+
+// if (trafficLight == "Red") {
+//     console.log("Stop Immediatedly.");
+// } else if (trafficLight == "Yellow") {
+//     console.log("Wait till the Light is green");
+// } else if (trafficLight == "Green") {
+//     console.log("You can go now.");
+// } else {
+//     console.log("Are you kidding? Fr?");
+// }
+
+// let marks = 99;
+// let grade1 = "A+";
+// let grade2 = "A";
+// let grade3 = "B";
+// let grade4 = "Fail";
+
+// if (marks > 80 && marks <= 100){
+//     console.log(`Your grade is ${grade1}`);
+// } else if (marks >= 60 && marks <= 80) {
+//     console.log(`Your grade is ${grade2}`);
+// } else if (marks >= 33 && marks <= 59) {
+//     console.log(`Your grade is ${grade3}`);
+// } else if (marks < 33) {
+//     console.log(`Your grade is ${grade4}`);
+// } else {
+//     console.log("Invalid Marks");
+// }
+
+// let size = "S";
+// XL = "250 Rs."
+// L = "200 Rs."
+// M = "100 Rs."
+// S = "50 Rs."
+
+// if (size === "XL"){
+//     console.log(`The Popcorn bill is ${XL}`);
+// } else if (size === "L") {
+//     console.log(`The Popcorn bill is ${L}`);
+// } else if (size === "M") {
+//     console.log(`The Popcorn bill is ${M}`);
+// } else if (size === "S") {
+//     console.log(`The Popcorn bill is ${S}`);
+// } else {
+//     console.log("Sorry, Size is not available.");
+// }
+
+// let marks = 90;
+
+// if (marks >= 33) {
+//     console.log("Pass");
+//     if (marks >= 80) {
+//         console.log("Grade : Outstanding");
+//     } else {
+//         console.log("Grade : A");
+//     }
+// } else if (marks < 33) {
+//     if (marks >= 28 && marks < 33) {
+//         console.log("Eligible For Grace Marks.");
+//     } else {
+//         console.log("Failed.");
+//     }
+// } else {
+//     console.log("Invalid Marks");
+// }
+
+// let m = 75;
+
+// if (m > 33 && m <= 80 || !false){
+//     console.log("pass");
+// }
+
+// let str = "apple";
+
+// if (str.length > 3 && str[0] === "a") {
+//   console.log(`${str} is a Good String.`);
+// } else {
+//   console.log(`${str} is not a Good String.`);
+// }
+
+// let num = 12;
+// if (num % 3 === 0 && (num + 1 == 15 || num - 1 == 11)) {
+//   console.log("Safe");
+// } else {
+//   console.log("Unsafe");
+// }
+
+// if ("") {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// if (" ") {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
+
+// Switch Statements :
+
+// let color = "red";
+
+// switch (color) {
+//   case "red":
+//     console.log("stop");
+//     break;
+//   case "yellow":
+//     console.log("slow down");
+//     break;
+//   case "green":
+//     console.log("GO");
+//     break;
+//   default:
+//     console.log("Broken Light");
+// }
+
+// let day = 4;
+
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Week has 7 days only");
+// }
+// alert("Hey there");
+// console.error("Error msg.");
+// console.warn("warning msg.");
+// let firstName = prompt("Enter Your First Name : ");
+// let lastName = prompt("Enter Your Last Name : ");
+// console.log(`Welcome ${firstName} ${lastName}nehal!`);
