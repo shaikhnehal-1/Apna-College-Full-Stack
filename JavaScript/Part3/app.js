0565bdd70995498e3ce6dc6efbb766d94fd532bb
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2025-03-15 22:18:54 +0530

    slice js

--- JavaScript/Part3/app.js
+++ JavaScript/Part3/app.js
@@ -28,8 +28,86 @@ console.log(str.indexOf("a"));
 
 let msg = "hellonehalshaikh";
 console.log(msg.slice(4, msg.length));
-console.log(msg.slice(-2, -1));
+console.log(msg.slice(-6, -1));
 
 let avg = "nehalshaikh";
-avg = avg.replace("neh", "beh");
+avg = avg.replace("n", "b");
+avg = avg.replace("ikh", "zia");
 console.log(avg);
+
+let fruitt = "Mango";
+console.log(fruitt.repeat(5));
+
+//Practice Q's
+let talk = "hello!";
+console.log(talk.trim().toUpperCase());
+
+let name = "ApnaCollege";
+console.log(name.slice(4, 9));
+console.log(name.indexOf("na"));
+console.log(name.replace("Apna", "Our"));
+console.log(name.slice(4).replace("l", "t").replace("l", "t"));
+
+
+//Array (Data Structure)
+let nums = [2, 4, 6, 8];
+console.log(nums[1]);
+
+let myInfo = ["Nehal", 21, 72.63];
+console.log(myInfo[0][0]);
+console.log(myInfo);
+myInfo[2] = 66.40;
+myInfo[10] = "Ehtesham";
+console.log(myInfo);
+
+//Array Methods : Pusp, Pop, Unshift, Shift
+let cars = ["BMW", "AUDI", "XUV", "PORSCHE"];
+console.log(cars);
+cars.pop();
+console.log(cars)
+cars.push("TOYOTA");
+console.log(cars);
+cars.shift();
+console.log(cars);
+cars.unshift("Ferrari");
+console.log(cars);
+
+let followers = ["ehtesham", "sufiyan", "amaan", "bhoomee"]
+console.log(followers);
+let blockedFoll = followers.shift();
+console.log(followers);
+console.log(blockedFoll);
+
+//practice Qs
+
+let months = ["january", "july", "march", "august"];
+console.log(months);
+months.shift();
+months.shift();
+console.log(months);
+months.unshift("june");
+months.unshift("july");
+console.log(months);
+
+let colors = ["red", "blue", "green"];
+//includes: search for a value.
+console.log(colors.includes("red"));
+
+//indexOf: returns index value in array
+console.log(colors.indexOf("green"));
+
+//concat: merge two arrays
+
+let colorsTwo = ["orange", "white", "black"];
+let colorsConcat = colors.concat(colorsTwo)
+console.log(colorsConcat)
+
+//reverse: reverse an array
+
+console.log(colorsConcat.reverse());
+
+//SLice
+console.log(colorsConcat.slice());
+console.log(colorsConcat.slice(2));
+console.log(colorsConcat.slice(2,3));
+console.log(colorsConcat.slice(-2));
